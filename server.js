c72036aa75c64e929fba46ea9812acff3734f861
Rachel Cianfichi <cianfich@gmail.com>
2021-10-18 18:20:45 -0400

    Added server.js file and package requirements.

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mysql = require('mysql2');
+const figlet = require('figlet');
+const inquirer = require ('inquirer');
+const table = require ('console.table');
+
