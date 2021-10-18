ab084df69caece5a608c51e9c3c7fd0014ca3a83
Rachel Cianfichi <cianfich@gmail.com>
2021-10-18 19:55:50 -0400

    Created art work for start of application and first question prompts.

--- server.js
+++ server.js
@@ -4,3 +4,28 @@ const figlet = require('figlet');
 const inquirer = require ('inquirer');
 const table = require ('console.table');
 
+const mainQuestions = () => {
+    return inquirer.prompt([
+        {
+          type: 'list',
+          name: 'mainQuestion',
+          message: 'What would you like to do?',
+          choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
+        },
+    ]);
+}
+
+
+
+const startProgram = () => {
+    console.log(figlet.textSync("Employee"))
+    console.log(figlet.textSync("Manager"))
+    console.log(figlet.textSync("System"))
+};
+
+const init = () => {
+    startProgram(); 
+    mainQuestions();
+ };
+
+init();
